23657568e66d594af952b4dba49a9ada2be16707
Alexander Lokaychuk <alex.lokaychuk@gmail.com>
2026-05-16 12:22:48 +0300

    Bump version to 2.0.0+6 (major) and build release APK

--- lib/core/version_info.dart
+++ lib/core/version_info.dart
@@ -8,10 +8,10 @@ class VersionInfo {
 
   /// Версия приложения (должна совпадать с pubspec.yaml).
   /// Формат: major.minor.patch
-  static const String version = '1.1.0';
+  static const String version = '2.0.0';
 
   /// Номер сборки.
-  static const int buildNumber = 5;
+  static const int buildNumber = 6;
 
   /// Полная строка версии для отображения.
   static String get displayVersion => 'v$version+$buildNumber';
